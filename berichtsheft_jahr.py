ca4bc42cb3fa548e859df6e7a52346878f746199
filip-golanski <45062263+filip-golanski@users.noreply.github.com>
2019-07-15 14:35:58 +0200

    vorher "jahr"-ordner erstellen

--- berichtsheft_jahr.py
+++ berichtsheft_jahr.py
@@ -20,19 +20,10 @@ for woche in range(1,54):
     daten = ["D0", "D1", "D2", "D3", "D4"]
 
     for n in range(5):
-            daten[n] = ((montag + datetime.timedelta(days=n)).strftime("%d.%m.%Y"))
+        daten[n] = ((montag + datetime.timedelta(days=n)).strftime("%d.%m.%Y"))
 
-    for zelle in document.tables[0]._cells:
-        if zelle.text == "DATUM0":
-            zelle.text = daten[0]
-        if zelle.text == "DATUM1":
-            zelle.text = daten[1]
-        if zelle.text == "DATUM2":
-            zelle.text = daten[2]
-        if zelle.text == "DATUM3":
-            zelle.text = daten[3]
-        if zelle.text == "DATUM4":
-            zelle.text = daten[4]
+        for zelle in document.tables[0]._cells:
+            zelle.text = zelle.text.replace("DATUM"+str(n), daten[n])
 
     for zelle in document.tables[1]._cells:
         zelle.text = zelle.text.replace("DATUM3",daten[3])
